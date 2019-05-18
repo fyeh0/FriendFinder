5f6a39b97dd6559f2200b20729d483bd72d2f307
fyeh0 <fangiyeh@gmail.com>
2019-05-17 23:24:08 -0400

    server file setup

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,17 +1,17 @@
-var express = require("express");
+const path = require("path");
 
-var app = express();
-var PORT = 3000;
+module.exports = function(app) {
+  var express = require("express");
 
-app.get("/", function(req, res) {
-    res.send("Welcome to Friend Finder!");
+  var app = express();
+  var PORT = 3000;
+
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "app/public/home.html"));
   });
-  
-app.get("/survey", function(req, res) {
-   res.sendFile(path.join(__dirname, "survey.html"))
+
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "app/public/survey.html"));
   });
 
-
- app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+};
--- server.js
+++ server.js
@@ -10,15 +10,9 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "app/public/home.html"));
-});
-
-app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "app/public/survey.html"))
-  });
-
+require ("./routing/htmlRoutes.js")(app)
+require ("./routing/apiRoutes.js")(app)
 
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+  console.log("App listening on PORT " + PORT);
+});
