aaed108fe9b2ddf72f07fff1f5fd887421cae5aa
fyeh0 <fangiyeh@gmail.com>
2019-05-21 23:31:04 -0400

    partially working match logic

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,10 +1,10 @@
 var friendArray = [
-    {
+  {
     name: "Kilgore Trout",
     photo: "http://lorempixel.com/400/200/animals/",
-    age: 1,
-    scores: [3,3,3,3,3,3,3,3,3,3]
-    },
-]
+    age: "1",
+    scores: ["3", "3", "3", "3", "3", "3", "3", "3", "3", "3"]
+  }
+];
 
 module.exports = friendArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const friendArray = require("../data/friends")
+const friendArray = require("../data/friends");
 
 module.exports = function(app) {
   app.get("/api/friends", function(req, res) {
@@ -8,8 +8,30 @@ module.exports = function(app) {
 
   app.post("/api/friends", function(req, res) {
     console.log(friendArray);
-    var newfriend= req.body;
-    console.log(newfriend);
+    // console.log(newfriend);
+
+    var newfriend = req.body;
+    var scores = newfriend.scores;
+
+    // var matchName = "";
+    // var matchImage = "";
+    var totalDiff = 10;
+
+    for (let i = 0; i < friendArray.length; i++) {
+      // friendArray.forEach(function(friend) {
+      var bestMatch = 0;
+      for (let j = 0; j < friendArray[i].scores[j]; j++) {
+        // friend.scores.forEach(function(userAnswers, i) {
+        // console.log(`Comparing ${friendArray.scores[j]} to ${friend.scores[i]}`);
+        bestMatch += Math.abs(friendArray[i].scores[i] - scores[j]);
+      }
+      if (bestMatch < totalDiff) {
+        totalDiff = bestMatch;
+        var matchName = friendArray[i].name;
+        var matchImage = friendArray[i].photo;
+      }
+    }
+
     friendArray.push(newfriend);
     res.json(newfriend);
   });
